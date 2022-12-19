17d995a08c0165acdee01c6766b763de29e68fc3
Yuwen Li <897194064@qq.com>
2022-12-20 05:49:39 +0800

    delete unused config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,17 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const ALCHEMY_API_KEY = "kfZc8hUzaZlAWtQ4s3EJJFv7E4bOwgtA";
-//const GOERLI_PRIVATE_KEY = ;
-
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true
-    },
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
     }
   },
   solidity: {
